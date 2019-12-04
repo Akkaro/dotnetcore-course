5afa77bacd91d74334ccd2bf733994184be06582
Akkaro <akos.beno@gmail.com>
2019-12-04 19:53:00 +0200

    aladar

---
+++ Trailers.MVC/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trailers.MVC.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Contract { get; set; }
+        public string Country { get; set; }
+
+    }
+}
